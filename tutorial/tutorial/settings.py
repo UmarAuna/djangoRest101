bcdb4e6962acd590c63701b61197f65e24d08f39
Umar Saidu Auna <umarmanofpeace@gmail.com>
2020-05-11 15:02:14 +0100

    fix: 🐛 URL

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-# Heroku: Update database configuration from $DATABASE_URL. 
+# Heroku: Update database configuration from $DATABASE_URL.
+import os
 import dj_database_url 
 
-import os
 
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,6 +85,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default']=db_from_env
 
 
 # Password validation
