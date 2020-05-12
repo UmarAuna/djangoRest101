c222c557ca502fb5f6fcee2e750f1365cff2eb03
Umar Saidu Auna <umarmanofpeace@gmail.com>
2020-05-12 14:59:21 +0100

    fix: 🐛 from django.core.urlresolvers import reverse

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.core.urlresolvers import reverse
 from django.urls import include, path
 from rest_framework import routers
 from quickstart import views
